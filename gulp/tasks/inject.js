41d74ab7d05b0be57f4354a81bb3ac87074c4b37
Elizabeth Simonian <esimonian16@gmail.com>
2016-09-11 22:21:58 +0700

    fully functional

--- gulp/tasks/inject.js
+++ gulp/tasks/inject.js
@@ -5,14 +5,17 @@ const inject = require('gulp-inject');
 // 'gulp inject:head' -- injects our style.css file into the head of our HTML
 gulp.task('inject:head', () =>
   gulp.src('.tmp/src/_includes/head.html')
-  // /assets/stylesheets/*.css
-    .pipe(inject(gulp.src('.tmp/josephmastey/assets/stylesheets/*.css'), {ignorePath: '.tmp'}))
+  // .pipe(inject(gulp.src('.tmp/assets/stylesheets/*.css'), {ignorePath: '.tmp'}))
+  // .pipe(inject(gulp.src('.tmp/assets/stylesheets/*.css'), {ignorePath: '.tmp', addPrefix: 'josephmastey'}))
+    .pipe(inject(gulp.src('.tmp/assets/stylesheets/*.css'), {ignorePath: '.tmp'}))
     .pipe(gulp.dest('.tmp/src/_includes'))
 );
 
 // 'gulp inject:footer' -- injects our index.js file into the end of our HTML
 gulp.task('inject:footer', () =>
   gulp.src('.tmp/src/_layouts/default.html')
-    .pipe(inject(gulp.src('.tmp/josephmastey/assets/javascript/*.js'), {ignorePath: '.tmp'}))
+  //.pipe(inject(gulp.src('.tmp/assets/javascript/*.js'), {ignorePath: '.tmp'}))
+  //.pipe(inject(gulp.src('.tmp/assets/javascript/*.js'), {ignorePath: '.tmp', addPrefix: 'josephmastey'}))
+    .pipe(inject(gulp.src('.tmp/assets/javascript/*.js'), {ignorePath: '.tmp'}))
     .pipe(gulp.dest('.tmp/src/_layouts'))
 );
